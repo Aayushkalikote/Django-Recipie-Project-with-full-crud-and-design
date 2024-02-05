36f9461ac8944b1e35a55c28b16352c67d2da990
Aayushkalikote <109607702+Aayushkalikote@users.noreply.github.com>
2024-02-05 21:47:56 +0545

    dynamic id get delete and table added in add form.

--- recipe/models.py
+++ recipe/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 
+
 # Create your models here.
 class Recipe(models.Model):
     recipe_name=models.CharField(max_length=100)
--- recipe/views.py
+++ recipe/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from recipe.models import Recipe
 from django.contrib import messages
+from django.shortcuts import get_object_or_404, redirect
+
 # Create your views here.
 def recipe(request):
-    return render(request, 'recipes.html')
+    queryselect=  Recipe.objects.all()  
+    context = {'recipes':queryselect}
+    return render(request, 'recipes.html',context)
 def add_recipe(request):
     if request.method=="POST":
         data=request.POST
@@ -12,4 +16,11 @@ def add_recipe(request):
         recipe_image=request.FILES.get('recipe_image')
         Recipe.objects.create(recipe_name=recipe_name,recipe_description=recipe_description,recipe_image=recipe_image)
         messages.success(request, "Recipe added successfully.")
-    return render(request,'add-recipes.html')
+    queryselect=  Recipe.objects.all()  
+    context = {'recipes':queryselect}    
+    return render(request,'add-recipes.html',context)
+def delete_recipe(request,id):
+    queryselect=Recipe.objects.get(id=id)
+    queryselect.delete()
+    messages.success(request, "Recipe Deleted successfully.")
+    return redirect('/add_recipes')
