6d0db0c80176318d52c63d94b0d611ae0076e5d9
Aayushkalikote <109607702+Aayushkalikote@users.noreply.github.com>
2024-02-06 16:24:46 +0545

    full recipe crud fixed from both front and backend

--- recipe/urls.py
+++ recipe/urls.py
@@ -5,6 +5,8 @@ from recipe import views
 
 urlpatterns = [
     path('', views.recipe,name='recipe'),
+    path('view_recipe/<id>/', views.view_recipe,name='view_recipe'),
     path('add_recipes', views.add_recipe, name='add_recipes'),
+    path('update_recipe/<id>/', views.update_recipe, name='update_recipe'),
     path('delete_recipe/<id>/', views.delete_recipe, name='delete_recipe'),
 ]
